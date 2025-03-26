564d3d847b16d2ff3f90b64f30a20ef4d3fd241a
shivaraj-bh <me@shivaraj-bh.in>
2025-03-26 17:55:16 +0530

    add delete-non-default-sg flake app

--- flake.nix
+++ flake.nix
@@ -33,6 +33,30 @@
         };
         modules = [ ./modules/terranix ];
       };
+      apps.delete-non-default-sg = {
+        type = "app";
+        program = pkgs.writeShellApplication {
+          name = "delete-non-default-sg";
+          runtimeInputs = [ pkgs.awscli2 ];
+          text = ''
+            SG_IDS=$(aws ec2 describe-security-groups \
+              --filters "Name=vpc-id,Values=$1" \
+              --region ap-south-1 \
+              --query "SecurityGroups[?GroupName!='default'].GroupId" \
+              --output text)
+            for SG_ID in $SG_IDS; do
+              echo "Deleting security group: $SG_ID"
+              aws ec2 delete-security-group --group-id "$SG_ID" --region "ap-south-1"
+            done
+          '';
+          meta.description = ''
+            Manually delete the non-default security-group from chutney's VPC.
+
+            `terraform destroy` only deletes the SG's managed by it. There can be other non-default SG's without
+            deleting which the VPC will not get destroyed.
+          '';
+        };
+      };
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           just
