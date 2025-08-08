72afb639068e8402dca04938c9433657866e8097
shivaraj-bh <me@shivaraj-bh.in>
2025-08-08 12:54:43 +0530

    import nixos' nixpkgs overlay

--- modules/flake-parts/integration-test.nix
+++ modules/flake-parts/integration-test.nix
@@ -1,6 +1,12 @@
 { inputs, ... }:
 {
-  perSystem = { pkgs, lib, ... }: {
+  perSystem = { inputs', pkgs, lib, ... }: {
+    # TODO: re-use ../../modules/nixos/nixpkgs.nix
+    nixpkgs.overlays = [
+      (_: _: {
+        attic-server = inputs'.attic.packages.attic-server;
+      })
+    ];
     # VM fails to boot successfully on `aarch64-linux`
     checks = lib.mkIf (pkgs.stdenv.isLinux && !pkgs.stdenv.isAarch64) {
       integration = pkgs.testers.runNixOSTest (import ../../tests/integration.nix inputs);
